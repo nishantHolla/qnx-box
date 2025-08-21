2579e1aa6c686f8b122729e8c35937ac109a9887
Nishant Holla <nishantholla19@gmail.com>
2025-08-21 09:31:09 +0530

    Add 3.lab_exec

---
+++ src/3.lab_exec.c
@@ -0,0 +1,32 @@
+/*
+ * 3.lab_exec.c
+ *
+ *  Created on: Aug 14, 2025
+ *      Author: nishant
+ */
+#include "qnx_lab.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int qnx_lab_exec(int argc, char** argv, char** envp) {
+  (void) argc, (void) argv, (void) envp;
+
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		execlp("ls", "ls", "-la", NULL);
+	}
+	else if (pid > 0) {
+		waitpid(pid, NULL, 0);
+		printf("Child terminated.\n");
+	}
+	else {
+		fprintf(stderr, "Failed to fork\n");
+	}
+
+	return EXIT_SUCCESS;
+}
+
