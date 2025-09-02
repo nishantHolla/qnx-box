431ff2c4eb2e90732f278e0446a6235630e3d132
Nishant Holla <nishantholla19@gmail.com>
2025-09-02 12:01:44 +0530

    Add 7.lab_thread

---
+++ src/7.lab_thread.c
@@ -0,0 +1,35 @@
+/*
+ * 7.lab_thread.c
+ *
+ *  Created on: Aug 26, 2025
+ *      Author: nishant
+ */
+#include "qnx_lab.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* thread_function(void* args) {
+	printf("thraed: Hello from the thread! Argument = %s\n", (char*)args);
+	return NULL;
+}
+
+int qnx_lab_thread(int argc, char** argv, char** envp) {
+	pthread_t thread;
+	const char* message = "QNX Thread Example";
+	printf("main: Creating thread...\n");
+
+	if (pthread_create(&thread, NULL, thread_function, (void*)message) != 0) {
+		fprintf(stderr, "main: Failed to create thread\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pthread_join(thread, NULL) != 0) {
+		fprintf(stderr, "main: Failed to join the thread");
+		exit(EXIT_FAILURE);
+	}
+
+	return EXIT_SUCCESS;
+}
