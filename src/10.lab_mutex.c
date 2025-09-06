c6078fcf4b8017b4fce8421ca058badf2f8a4177
Nishant Holla <nishantholla19@gmail.com>
2025-09-06 11:22:27 +0530

    Update 10.lab_mutex

--- src/10.lab_mutex.c
+++ src/10.lab_mutex.c
@@ -4,6 +4,8 @@
  *  Created on: Sep 2, 2025
  *      Author: nishant
  */
+#include "qnx_lab.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -11,46 +13,20 @@
 #include <string.h>
 #include <stdbool.h>
 
-int counter = 0;
-pthread_mutex_t lock;
+static int counter = 0;
+static pthread_mutex_t lock;
 
-void* thread_func_sync(void* arg) {
-  pthread_t id = pthread_self();
-
-	for (int i = 0; i < 5; ++i) {
-		pthread_mutex_lock(&lock);
-
-		int local = counter;
-		printf("Thread %d: counter = %d\n", (int)id, local);
-		counter = local + 1;
-
-		pthread_mutex_unlock(&lock);
-	}
-	return NULL;
-}
-
-void* thread_func_no_sync(void* arg) {
-  pthread_t id = pthread_self();
-
-	for (int i = 0; i < 5; ++i) {
-		int local = counter;
-		printf("Thread %d: counter = %d\n", (int)id, local);
-		counter = local + 1;
-	}
-	return NULL;
-}
+static void* thread_work(void* arg);
 
 int qnx_lab_mutex(int argc, char** argv, char** envp) {
-  (void) envp;
-
-  bool use_mutex = true;
+  int use_mutex = 1;
 
   if (argc == 3) {
     if (strcmp(argv[2], "sync") == 0) {
-      use_mutex = true;
+      use_mutex = 1;
     }
     else if (strcmp(argv[2], "no_sync") == 0) {
-      use_mutex = false;
+      use_mutex = 0;
     }
     else {
       fprintf(stderr, "Usage: a.out [sync|no_sync]\n");
@@ -63,20 +39,18 @@ int qnx_lab_mutex(int argc, char** argv, char** envp) {
   if (use_mutex)  {
     printf("Using mutex\n");
 
-    if (pthread_mutex_init(&lock, NULL) != 0) {
-      fprintf(stderr, "Failed to init mutex\n");
-      return 1;
+    int ret = pthread_mutex_init(&lock, NULL) != 0;
+    if (ret != 0) {
+      fprintf(stderr, "Failed to init mutex. Error: %s\n", strerror(ret));
+      exit(EXIT_FAILURE);
     }
-
-    pthread_create(&t1, NULL, thread_func_sync, NULL);
-    pthread_create(&t2, NULL, thread_func_sync, NULL);
   }
   else {
     printf("Not using mutex\n");
-
-    pthread_create(&t1, NULL, thread_func_no_sync, NULL);
-    pthread_create(&t2, NULL, thread_func_no_sync, NULL);
   }
+
+  pthread_create(&t1, NULL, thread_work, &use_mutex);
+  pthread_create(&t2, NULL, thread_work, &use_mutex);
 
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
@@ -84,8 +58,40 @@ int qnx_lab_mutex(int argc, char** argv, char** envp) {
   printf("Final counter: %d\n", counter);
 
   if (use_mutex) {
-    pthread_mutex_destroy(&lock);
+    int ret = pthread_mutex_destroy(&lock);
+    if (ret != 0) {
+      fprintf(stderr, "pthread_mutex_destroy failed. Error: %s\n", strerror(ret));
+      exit(EXIT_FAILURE);
+    }
   }
-	return 0;
+
+	return EXIT_SUCCESS;
 }
 
+static void* thread_work(void* arg) {
+  int use_mutex = *(int *) arg;
+  int id = pthread_self();
+
+	for (int i = 0; i < 5; ++i) {
+    if (use_mutex) {
+      int ret = pthread_mutex_lock(&lock);
+      if (ret != 0) {
+        fprintf(stderr, "pthread_mutex_lock failed. Error: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+      }
+    }
+
+		int local = counter;
+		printf("Thread %d: counter = %d\n", (int)id, local);
+		counter = local + 1;
+
+    if (use_mutex) {
+      int ret = pthread_mutex_unlock(&lock);
+      if (ret != 0) {
+        fprintf(stderr, "pthread_mutex_unlock failed. Error: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+      }
+    }
+	}
+	return NULL;
+}
